157de64d7598aaacd68cb25a119ed10c610c4281
codepageme <codepage.me@gmail.com>
2025-01-27 21:54:14 +0100

    redeployme

--- api/dist/greeting.dev.js
+++ api/dist/greeting.dev.js
@@ -7,8 +7,8 @@ exports["default"] = handler;
 
 function handler(req, res) {
   var _req$query$name = req.query.name,
-      name = _req$query$name === void 0 ? "World" : _req$query$name;
-  var message = "Hello, '".concat(name, "'! We hope you like this endpoint \uD83D\uDC4D\u2764\uFE0F. Happy coding!");
+      name = _req$query$name === void 0 ? "codex" : _req$query$name;
+  var message = "Hello, '".concat(name, "'! We hope you like this endpoint \uD83D\uDC4D\u2764\uFE0F. Happycode!");
   console.log("Generated message:", message); // Log the final message
 
   res.status(200).json({
--- api/greeting.js
+++ api/greeting.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
-    const { name = "World" } = req.query;
-    const message = `Hello, '${name}'! We hope you like this endpoint 👍❤️. Happy coding!`;
+    const { name = "codex" } = req.query;
+    const message = `Hello, '${name}'! We hope you like this endpoint 👍❤️. Happycode!`;
     
     console.log("Generated message:", message); // Log the final message
     res.status(200).json({ message });
