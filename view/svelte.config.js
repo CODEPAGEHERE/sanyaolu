11bdaadc0443f0ad559aae72613b979503d24e45
codepageme <codepage.me@gmail.com>
2025-08-06 03:39:10 +0100

    vercel build

--- view/svelte.config.js
+++ view/svelte.config.js
@@ -1,9 +1,10 @@
 import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default {
-  preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter()
-  }
+    adapter: adapter({
+      // options
+    }),
+    outDir: 'view',
+  },
 };
